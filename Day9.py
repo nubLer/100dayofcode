40aa187b1c7314081943bed8f05ff24bf0bd5abb
David Stevens <dstevens745@gmail.com>
2024-01-15 11:12:32 -0800

    Day9stuff

--- Day9.py
+++ Day9.py
@@ -1,10 +1,14 @@
 # Write your code below this line 👇
 def prime_checker(number):
-    if number / 1 == number and number / number == 1 and number is not 2:
-        if number // 2 + 1 == number - 1:
+    if number == 2:
+         print(f"{number} is a prime number.")
+    elif number > 2:
+        x = number // 2 + 1 
+        y = x * 2 - 1
+        if number == y:
             print(f"{number} is a  prime number. ")
-    elif number == 2:
-        print(f"{number} is a  prime number. ")
+        else:
+            print(f"{number} is not a prime number") 
     else:
         print(f"{number} is not a prime number")    
 
@@ -14,5 +18,11 @@ def prime_checker(number):
 # Write your code above this line 👆
     
 #Do NOT change any of the code below👇
-n = 10 # Check this number
+n = 87 # Check this number
 prime_checker(number=n)
+
+
+#Chat GPT's answer is below - 
+# Write your code above this line 👆
+
+
