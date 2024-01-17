47e8dbe0b56c549535d0dec114f1b4dbb3d2b789
David Stevens <dstevens745@gmail.com>
2024-01-16 18:30:35 -0800

    commit stuff?

--- Day8pt1.py
+++ Day8pt1.py
@@ -1,16 +1,16 @@
 # Write your code below this line 👇
 def prime_checker(number):
     if number == 2:
-         print(f"{number} is a prime number.")
+         print("It's a prime number.")
     elif number > 2:
         x = number // 2 + 1 
         y = x * 2 - 1
         if number == y:
-            print(f"{number} is a  prime number. ")
+            print("It's a prime number.")
         else:
-            print(f"{number} is not a prime number") 
+            print("It's not a prime number.") 
     else:
-        print(f"{number} is not a prime number")    
+        print("It's not a prime number.")    
 
 
 
@@ -22,7 +22,22 @@ n = 87 # Check this number
 prime_checker(number=n)
 
 
-#Chat GPT's answer is below - 
+#Chat GPT's answer is below - same as instructors : 
 # Write your code above this line 👆
 
 
+# Write your code below this line 👇
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+      if number % i == 0:
+          is_prime = False
+    if is_prime:  
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+# Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+prime_checker(number=n)
